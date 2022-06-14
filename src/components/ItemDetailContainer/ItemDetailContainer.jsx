bcd97e7ef91578cd71014e91be8ac96859fb0620
fuentesjulian <fuentes.julian@hotmail.com>
2022-06-13 23:36:25 -0300

    Replacing the array filter by array find in ItemDetailContainer

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -5,14 +5,14 @@ import ItemDetail from "../ItemDetail/ItemDetail.jsx";
 
 function ItemDetailContainer() {
   const { id } = useParams();
-  const [item, setItem] = useState([]);
+  const [item, setItem] = useState();
 
   const myUrl = "https://run.mocky.io/v3/ab3ab053-792c-406b-8524-eb0e70f07b3f";
   const fetchItem = (url, id) => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        setItem(data.filter((item) => item.id === parseInt(id)));
+        setItem(data.find((item) => item.id === parseInt(id)));
       })
       .catch((error) => console.log(error));
   };
@@ -20,7 +20,7 @@ function ItemDetailContainer() {
     fetchItem(myUrl, id);
   }, []);
 
-  return <>{item.length > 0 ? <ItemDetail item={item[0]} /> : <>Loading</>}</>;
+  return <>{(item)? <ItemDetail item={item} /> : <>Loading</>}</>;
 }
 
 export default ItemDetailContainer;
