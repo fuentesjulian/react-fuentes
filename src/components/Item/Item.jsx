83282aa4f569aa6eef3aab52959713163bf381f4
fuentesjulian <fuentes.julian@hotmail.com>
2022-07-01 14:00:57 -0300

    Optimizing item list styles

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -8,7 +8,7 @@ function Item({ item }) {
   const formattedPrice = price.toLocaleString();
 
   return (
-    <div className="itemCard">
+    <div className={stock > 0 ? "itemCard enabled" : "itemCard disabled"}>
       <img className="card-img" src={image} alt="Card image cap" />
       <div className="itemCard-body">
         <div className="itemCard-bodyTop">
@@ -16,9 +16,15 @@ function Item({ item }) {
         </div>
         <div className="itemCard-bodyBottom">
           <div className="itemCard-price">$ {formattedPrice}</div>
-          <div className="itemCard-stock">{stock > 0 ? <>Stock: {stock} unidades</> : <>Sin stock</>}</div>
+          <div className="itemCard-stock">Stock: {stock} unidades</div>
           <div className="col-md-12 text-center itemCard-btn">
-            {(stock>0)?<Link className="btn-buy" to={`/item/${id}`}>Comprar</Link>:<div className="btn-buy-disabled">Comprar</div>}
+            {stock > 0 ? (
+              <Link className="btn-buy" to={`/item/${id}`}>
+                Comprar
+              </Link>
+            ) : (
+              <div className="btn-buy-disabled">Sin stock</div>
+            )}
           </div>
         </div>
       </div>
